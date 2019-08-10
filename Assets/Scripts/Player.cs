5c683aafe72ae35c30f90fd104a1f82b30c239a4
Michael Perez <mperez980089@yahoo.com>
2019-08-10 13:25:00 -0700

    Implemented player movement, collector, temp graphics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float pushForce;
+    public float rotateSpeed;
 
     Rigidbody2D rb;
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        float rotateDirection = Input.GetAxisRaw("Horizontal");
+        transform.Rotate(new Vector3(0, 0, (rotateSpeed * -rotateDirection) * Time.deltaTime));
+
+        //Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+        //rigidbody2D.AddForce(dir * force);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            rb.AddForce(transform.up * pushForce);
+        }
+    }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Collector"))
+        {
+            print("yay");
         }
     }
 }
