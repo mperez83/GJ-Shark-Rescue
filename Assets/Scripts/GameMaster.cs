c4069ffa627fee963199d25fed1a73e58a132b03
Michael Perez <mperez980089@yahoo.com>
2019-08-10 12:34:30 -0700

    Actual initial commit

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster instance;
+
+    [HideInInspector]
+    public float highScore = 0;
+
+    [HideInInspector]
+    public float screenTopEdge;
+    [HideInInspector]
+    public float screenBottomEdge;
+    [HideInInspector]
+    public float screenLeftEdge;
+    [HideInInspector]
+    public float screenRightEdge;
+
+    /*[HideInInspector]
+    public MusicMaster musicMaster;*/
+
+
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+
+        Cursor.lockState = CursorLockMode.Confined;
+
+        /*musicMaster = GetComponent<MusicMaster>();
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "MainMenu":
+                musicMaster.PlayMenuMusic();
+                break;
+
+            case "Game":
+                musicMaster.PlayGameMusic();
+                break;
+        }*/
+    }
+
+    void Update()
+    {
+        screenTopEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, -(Camera.main.transform.position.z))).y;
+        screenBottomEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, -(Camera.main.transform.position.z))).y;
+        screenLeftEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, -(Camera.main.transform.position.z))).x;
+        screenRightEdge = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, -(Camera.main.transform.position.z))).x;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float pushForce;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
